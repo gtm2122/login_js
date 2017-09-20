e838740e603f82c10ac8faa8281c3aadef4d6008
gtm2122 <gabrielthomas281@gmail.com>
2017-09-20 17:29:42 -0400

    made basic login page

---
+++ lol.js
@@ -0,0 +1,10 @@
+function check(form){
+        if(form.userid.value =="gg" && form.pswrd.value=="123")
+        {
+          window.open('https://google.com')
+        }
+        else
+        {
+          alert("Wrong Username or Password")
+        }
+      }
